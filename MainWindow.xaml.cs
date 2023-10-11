0f275de1e7d25e159e60b484c100a2ec7c29b01c
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-10-11 13:49:24 -0700

    Finished Message Box Question

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -31,14 +31,66 @@ namespace GA_2_WillCram
         
         private void btnFullName_Click(object sender, RoutedEventArgs e)
         {
+            DisplayFullName();
 
         } // btnFullName_Click
 
+        // Organizing my code into methods
+        public void DisplayFullName()
+        {
+            // Declare two string variables to hold our first and last name
+            //bool firstNameNotEmpty = txtFirstName.Text != "";
+            //bool lastNameNoteEmpty = txtLastName.Text != "";
+
+            bool nameBoxesNotEmpty = ValidateTextBoxes(txtFirstName.Text, txtLastName.Text);
+
+            if (nameBoxesNotEmpty)
+            {
+                string firstName = txtFirstName.Text;
+                string lastName = txtLastName.Text;
+
+                // Declare a third string, full name
+                // Concatenate ( combine ) the first and last name into one long string
+                string fullName = $"{firstName} {lastName}";
+
+                // Display full name on button click
+                MessageBox.Show(fullName);
+            }
+            else
+            {
+                MessageBox.Show("Please enter your first and last name in the text boxes");
+            }
+        } // DisplayFullName
+
+        public bool ValidateTextBoxes(string textBox1, string textBox2)
+        {
+            bool textBox1NotEmpty = textBox1 != "";
+            bool textBox2NotEmpty = textBox2 != "";
+
+            return textBox1NotEmpty && textBox2NotEmpty;
+        } // ValidateTextBoxes
+
+
+
         // Add or Subtract Two Numbers - Display in a text box
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSubtract_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
 
 
         // Display a single character from a Word in a Label
 
+        private void btnDisplayLetter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
 
     } // class
 
